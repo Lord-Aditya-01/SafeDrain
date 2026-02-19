12c26297c41edf1a7ab5ac4a404dcf0494ea034b
Lord-Aditya-01 <adityagirhe83@gmail.com>
2026-02-19 19:43:54 +0530

    improving supervisor dashboard Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import WorkerLogin from "./pages/WorkerLogin";
 import WorkerSignup from "./pages/WorkerSignup";
 import WorkerPage from "./pages/WorkerPage";
+
 import SupervisorPage from "./pages/SupervisorPage";
 import SupervisorLogin from "./pages/SupervisorLogin";
+
 import AppInitializer from "./AppInitializer";
+
 import SupervisorWorkers from "./components/supervisor/SupervisorWorkers";
 import SupervisorDashboard from "./pages/supervisor/SupervisorDashboard";
-
+import SupervisorAlerts from "./components/supervisor/SupervisorAlerts"; // ⭐ MISSING IMPORT
 
 function App() {
   return (
     <BrowserRouter>
       <AppInitializer />
+
       <Routes>
+
+        {/* Worker Routes */}
         <Route path="/" element={<WorkerLogin />} />
         <Route path="/signup" element={<WorkerSignup />} />
         <Route path="/worker" element={<WorkerPage />} />
-        <Route path="/supervisor" element={<SupervisorPage />} />
+
+        {/* Supervisor Routes */}
         <Route path="/supervisor-login" element={<SupervisorLogin />} />
-        <Route path="/supervisor" element={<SupervisorPage />} />
-        {/* <Route path="/supervisor-dashboard" element={<SupervisorDashboard />} /> */}
-        <Route path="/supervisor/workers" element={<SupervisorWorkers />} />
-        <Route path="/supervisor-alerts" element={<AlertsPage />} />
+
         <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
-        <Route path="/supervisor/alerts" element={<SupervisorAlerts />} /> 
+        <Route path="/supervisor/workers" element={<SupervisorWorkers />} />
+        <Route path="/supervisor/alerts" element={<SupervisorAlerts />} />
+
       </Routes>
     </BrowserRouter>
   );
